64795612c41c56bb2d648b375cfa8a3c410560d3
troii <lolitunbina@gmail.com>
2026-01-19 20:50:27 +0700

    input

--- input-sistem.cpp
+++ input-sistem.cpp
@@ -5,10 +5,10 @@ int main()
     std::string name;
     int age;
 
-    std::cout << "masukkan namamu: ";
+    std::cout << "Masukkan namamu: ";
     std::cin >> name;
 
-    std::cout << "Berapa umurmu: ";
+    std::cout << "Berapa Umurmu: ";
     std::cin >> age;
 
     std::cout << "Halo " << name << " berumur " << age << " tahun";
