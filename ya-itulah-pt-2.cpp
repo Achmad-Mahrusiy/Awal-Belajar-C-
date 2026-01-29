e3ed8314c82bc379ea21c51d7b0e897f9348d9cf
troii <lolitunbina@gmail.com>
2026-01-29 23:28:31 +0700

    ya itulah part 2

---
+++ ya-itulah-pt-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctime>
+
+int main()
+{
+    srand(time(0));
+    int randNum = rand() % 4 + 1;
+
+    switch (randNum)
+    {
+    case 1:
+        std::cout << "Beginilah hidup bro!";
+        break;
+    case 2:
+        std::cout << "When yah";
+        break;
+    case 3:
+        std::cout << "SAWIT SAWIT SAWIT!!!";
+        break;
+    case 4:
+        std::cout << "19 juta lapangan pekerjaan";
+        break;
+    }
+    return 0;
+}
