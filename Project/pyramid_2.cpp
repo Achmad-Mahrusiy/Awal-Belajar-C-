63a53732cf50ba8b7355aa0338d2bf3249bcd4d9
troii <lolitunbina@gmail.com>
2026-02-03 22:59:47 +0700

    type pyramid bentuk $

---
+++ Project/pyramid_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int tinggi;
+    std::cout << "Masukkan tinggi piramida: ";
+    std::cin >> tinggi;
+
+    for (int i = 1; i <= tinggi; i++) {
+        for (int j = 1; j <= tinggi - i; j++) {
+            std::cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            std::cout << "$";
+        }
+
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
