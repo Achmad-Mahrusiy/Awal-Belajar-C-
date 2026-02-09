f78863da240c3a550e8f312d28873fb260bb3cf4
troii <lolitunbina@gmail.com>
2026-02-09 21:57:02 +0700

    day 3 coba login

--- Project/login_coba_coba.cpp
+++ Project/login_coba_coba.cpp
@@ -29,7 +29,7 @@ void login(std::string username)
     std::cin >> username;
 
     std::cout << '\n';
-    std::cout << username << " Selamat Datang " << "di Fakultas Ilmu Komputer\n\n";
+    std::cout << "Selamat Datang " << username << " di Fakultas Ilmu Komputer\n\n";
 }
 
 void prodi(int jurusan, int angkatan)
@@ -46,13 +46,85 @@ void prodi(int jurusan, int angkatan)
         switch (jurusan)
         {
         case 1:
-            std::cout << "Selamat datang di Sistem Informasi\n";
+            std::cout << "anda mahasiswa angkatan berapa: ";
+            std::cin >> angkatan;
+            if (angkatan == 2025)
+            {
+                std::cout << "Selamat datang mahasiswa baru " << angkatan << '\n';
+                std::cout << "Berikut Materi Materi yang keluar di semester 2 di Sistem Informasi: \n";
+                std::cout << "1. \n";
+                std::cout << "2. \n";
+                std::cout << "3. \n";
+                std::cout << "4. \n";
+                std::cout << "5. \n";
+                std::cout << "6. \n";
+                std::cout << "7. \n";
+                std::cout << "8. \n";
+                break;
+            }
+            else if (angkatan == 2024)
+            {
+                std::cout << "Selamat datang di Sistem Informasi angkatan " << angkatan << '\n';
+                std::cout << "Berikut Materi Materi yang keluar di semester 4 di Sistem Informasi: \n";
+                std::cout << "1. \n";
+                std::cout << "2. \n";
+                std::cout << "3. \n";
+                std::cout << "4. \n";
+                std::cout << "5. \n";
+                std::cout << "6. \n";
+                std::cout << "7. \n";
+                std::cout << "8. \n";
+                break;
+            }
+            else if (angkatan < 2024)
+            {
+                std::cout << "Welcome back mahasiswa old";
+            }
+            else
+            {
+                std::cout << "Mohon masukkan tahun angkatan yang belum lulus";
+            }
             break;
         case 2:
-            std::cout << "Selamat datang di Teknologi Informasi\n";
+            if (angkatan == 2025)
+            {
+                std::cout << "Selamat datang mahasiswa baru " << angkatan << '\n';
+                break;
+            }
+            else if (angkatan == 2024)
+            {
+                std::cout << "Selamat datang di Teknologi Informasi angkatan " << angkatan << '\n';
+                break;
+            }
+            else if (angkatan < 2024)
+            {
+                std::cout << "Welcome back mahasiswa old";
+            }
+            else
+            {
+                std::cout << "Mohon masukkan tahun angkatan yang belum lulus";
+            }
+            break;
             break;
         case 3:
-            std::cout << "Selamat datang di Informatika\n";
+            if (angkatan == 2025)
+            {
+                std::cout << "Selamat datang mahasiswa baru " << angkatan << '\n';
+                break;
+            }
+            else if (angkatan == 2024)
+            {
+                std::cout << "Selamat datang di Informatika angkatan " << angkatan << '\n';
+                break;
+            }
+            else if (angkatan < 2024)
+            {
+                std::cout << "Welcome back mahasiswa old";
+            }
+            else
+            {
+                std::cout << "Mohon masukkan tahun angkatan yang belum lulus";
+            }
             break;
         default:
             std::cout << "Masukkan Prodi yang sudah tertera!\n";
